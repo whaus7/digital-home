efeecd243056a6be3ab96d7cee886f7c6ca28f4b
whaus777 <whaus777@gmail.com>
2025-09-01 15:12:39 -0700

    buiuld fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Libre_Baskerville } from "next/font/google";
 import { Montserrat } from "next/font/google";
-import "./globals.css";
+import "@/app/global.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
